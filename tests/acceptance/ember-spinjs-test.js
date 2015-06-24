84bc03baf058953f0c6d46235babf1bbef77923f
Dave Laird <dlaird@godaddy.com>
2015-06-24 09:01:22 -0700

    remove extra code

--- tests/acceptance/ember-spinjs-test.js
+++ tests/acceptance/ember-spinjs-test.js
@@ -1,28 +1,3 @@
-// import Ember from 'ember';
-// import {module, test} from 'qunit';
-// import startApp from '../helpers/start-app';
-//
-// var App, server;
-//
-// module('Acceptance: Admin', {
-//   beforeEach: function() {
-//     App = startApp();
-//   },
-//   afterEach: function() {
-//     Ember.run(App, 'destroy');
-//   }
-// });
-//
-// asyncTest('three spinners are displayed', function(assert) {
-//   assert.expect(1);
-//   visit('/');
-//   Ember.run.later(this, function() {
-//     start();
-//     assert.equal(find('.spinner-display').length, 5);
-//   }, 0);
-// });
-
-
 import Ember from 'ember';
 import { module, test } from 'qunit';
 import startApp from '../helpers/start-app';
