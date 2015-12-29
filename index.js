40f51e0d030c7350bce25fba1e1ab82a27187ba2
Joe <joedaniels29@me.com>
2015-12-28 21:32:32 -0500

    Quick fix for nested add-on

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ module.exports = {
   name: 'ember-cli-spinjs',
 
   included: function(app) {
+     // see: https://github.com/ember-cli/ember-cli/issues/3718
+    if (typeof app.import !== 'function' && app.app) {
+      app = app.app;
+    }
     this._super.included(app);
 
     app.import(app.bowerDirectory + '/spin.js/spin.js');
