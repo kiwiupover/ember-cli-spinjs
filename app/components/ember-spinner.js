eab0e1c64904438e8b6103bf0edb8b95b6d56299
Dave Laird <kiwiupover@gmail.com>
2017-10-09 22:52:37 -0700

    Upgraded to ember-cli 2.16.0

--- app/components/ember-spinner.js
+++ app/components/ember-spinner.js
@@ -1,6 +1,1 @@
-/* global Spinner */
-
-import Ember from 'ember';
-import emberSpinner from 'ember-cli-spinjs/components/ember-spinner';
-
-export default emberSpinner;
+export { default } from 'ember-cli-spinjs/components/ember-spinner';
