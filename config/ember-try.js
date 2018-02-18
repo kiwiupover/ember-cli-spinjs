ec66d02f02bdb34968ad2874c2f399c4e397a8dd
Dave Laird <kiwiupover@gmail.com>
2018-02-17 20:43:26 -0800

    Update circleci config

--- config/ember-try.js
+++ config/ember-try.js
@@ -3,6 +3,17 @@ module.exports = {
   useYarn: true,
   scenarios: [
     {
+      name: 'ember-1-13',
+      bower: {
+        dependencies: {
+          'ember': '~1.13.0'
+        },
+        resolutions: {
+          'ember': '~1.13.0'
+        }
+      }
+    },
+    {
       name: 'ember-lts-2.12',
       npm: {
         devDependencies: {
